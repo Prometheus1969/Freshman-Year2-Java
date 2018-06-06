0575a0c7da69f7520d9a2ec84683fe1578075ea2
Prometheus <39853790+Prometheus1969@users.noreply.github.com>
2018-06-06 16:51:38 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+	public static void main(String args[]){
+		A a = new Print();
+		B b = new Print();
+		a.printCapitalLetter();
+		System.out.println("\n");
+		b.printLowercaseLetter();
+	}
+}
